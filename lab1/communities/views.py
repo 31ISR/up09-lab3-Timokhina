5cd3af0c0698cd98f62eb815c7a41b4c9b33c8bd
ST3504 <ST3504@TE.local>
2024-12-16 16:58:15 +0600

    Завершила всю 2-ую лабораторную работуgit add .git add .! ВСЁ РАБОТАЕТgit add .

--- lab1/communities/views.py
+++ lab1/communities/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Communities
-#def communities_list(req):
-    #return render(req, 'communities/communities_list.html')
+
 
 
 def communities_list(request):
     commun = Communities.objects.all().order_by('-date')
-    return render(request, 'communities/communities_list.html', {'commun': Communities})
+    return render(request, 'communities/communities_list.html', {'commun': commun})
+
+def community_page(request, slug):
+    community = Communities.objects.get(slug=slug)
+    return render(request, 'communities/community_page.html', {'community': community})
