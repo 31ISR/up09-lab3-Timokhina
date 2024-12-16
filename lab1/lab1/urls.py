31ff5e4e3e9ddcd6498a0d38302f6638c38a2b43
ST3504 <ST3504@TE.local>
2024-12-16 18:04:57 +0600

    на первом пункте добавила в  urls.py чо та там

--- lab1/lab1/urls.py
+++ lab1/lab1/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.urls import path, include 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,4 @@ urlpatterns = [
     path('communities/', include('communities.urls')),
     path('layout/', views.layout),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
